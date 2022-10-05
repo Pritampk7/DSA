7faa64f58dcfdb0aecd167031a11ca61a96e1fe5
Apple <apple@Apples-MacBook-Pro.local>
2022-10-05 18:11:22 +0530

    maxsum

---
+++ maxsumofsubarray.py
@@ -0,0 +1,16 @@
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+def maxsumofthesubarray(arr):
+
+
+    maxsum = arr[0]
+    temp_sum = 0
+    for item in arr:
+        if temp_sum < 0:
+            temp_sum = 0
+        temp_sum += item
+        maxsum = max(maxsum, temp_sum)
+
+    return maxsum
+
+print(maxsumofthesubarray(arr))
