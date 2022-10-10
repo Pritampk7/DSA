11187a4ebdef862cb3d062c34caee6b2d4415731
Apple <apple@Apples-MacBook-Pro.local>
2022-10-10 11:22:57 +0530

    Revert "test"
    This reverts commit d6668493cd8aa28fb55e880899c89c4ff4256e74.

--- maxsumofsubarray.py
+++ maxsumofsubarray.py
@@ -3,7 +3,7 @@ arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
 def maxsumofthesubarray(arr):
 
-    maxsum = arr[0] #get first member
+    maxsum = arr[0]
     temp_sum = 0
 
     for item in arr:
@@ -13,5 +13,6 @@ def maxsumofthesubarray(arr):
         maxsum = max(maxsum, temp_sum)
 
     return maxsum
+
 
 print(maxsumofthesubarray(arr))
