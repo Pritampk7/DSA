a623736beff339aafa7279ef3b6028fad74d0023
Apple <apple@Apples-MacBook-Pro.local>
2022-10-10 11:20:52 +0530

    Revert "maxproductofsubarray"
    This reverts commit 6dcbb435c21a0101ef5575cb0d75fecc720a58fe.

--- maxsumofsubarray.py
+++ maxsumofsubarray.py
@@ -2,6 +2,7 @@ arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
 
 def maxsumofthesubarray(arr):
+
     maxsum = arr[0]
     temp_sum = 0
 
--- twosum.py
+++ twosum.py
@@ -1,6 +1,7 @@
 arr = [2, 7, 11, 15]
 target = 9
 
+
 def twosum(arr, target):
     values = {}  # value:index
     for index, value in enumerate(arr):
