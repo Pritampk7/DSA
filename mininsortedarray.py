c977abb3c5ff4b4aaa1b209109ab2d9ce23e92b6
Apple <apple@Apples-MacBook-Pro.local>
2022-10-17 14:54:06 +0530

    maxproductofsubarray

---
+++ mininsortedarray.py
@@ -0,0 +1,18 @@
+def findmin(arr):
+    res = arr[0]
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        if arr[l] < arr[r]:
+            res = min(res, arr[l])
+            return res
+        mid = (l + r) // 2
+        res = min(res, arr[mid])
+        if arr[mid] >= arr[l]:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return res
+
+
+arr = [4, 5, 7, 6, 1, 2]
+print(findmin(arr))
